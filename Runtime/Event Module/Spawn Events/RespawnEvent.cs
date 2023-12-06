74c7e97da0ba3b631c097e8bad3099a4102fdeb0
crowhound <jonathan.westfall1@gmail.com>
2023-12-06 12:21:38 -0600

    Started working on the respawn event structs.

---
+++ Runtime/Event Module/Spawn Events/RespawnEvent.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace SF.Events
+{
+    public enum RespawnEventTypes
+    {
+        InitialRespawn, // First Spawn of the scene loading.
+        RespawnAll,
+        PlayerRespawnAll, // This maily be used for multiplayer set ups.
+        ItemsRespawnAll, //Items, Collectables, Interactables, and so forth.
+        PlayerRespawn,
+        GameObjectRespawn, // This is used for respawning a single specific game object of any type.
+    }
+    public struct RespawnEvent
+    {
+        public RespawnEventTypes EventType;
+        public GameObject RespawnGameObject;
+        public RespawnEvent(RespawnEventTypes eventType, GameObject respawnGameObject = null)
+        {
+            EventType = eventType;
+            RespawnGameObject = respawnGameObject;
+        }
+        static RespawnEvent respawnEvent;
+
+         public static void Trigger(RespawnEventTypes eventType, GameObject respawnGameObject = null)
+         {
+            respawnEvent.EventType = eventType;
+            respawnEvent.RespawnGameObject = respawnGameObject;
+         }
+    }
+}

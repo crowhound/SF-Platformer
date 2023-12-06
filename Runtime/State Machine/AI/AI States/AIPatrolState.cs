e7b5875f5ddbac276e44f92119d623be8b14b074
crowhound <jonathan.westfall1@gmail.com>
2023-12-06 13:55:22 -0600

    Added some get component calls to set the controller and character2d for the AI Patrol script.

--- Runtime/State Machine/AI/AI States/AIPatrolState.cs
+++ Runtime/State Machine/AI/AI States/AIPatrolState.cs
@@ -1,3 +1,4 @@
+using SF.Characters;
 using SF.Characters.Controllers;
 
 using UnityEngine;
@@ -7,14 +8,17 @@ namespace SF.AI
 	[CreateAssetMenu(fileName = "AI Patrol", menuName = "SF /Data/States/AI/AI Patrol")]
 	public class AIPatrolState : AIState
 	{
+		public bool DoTurnOnCollision = true;
 		protected override void OnInit()
 		{
-
+			controller = AIBrain.GetComponent<Controller2D>();
+			character = AIBrain.GetComponent<Character2D>();
+			controller.Direction.x = 1;
 		}
 
 		protected override void OnUpdateState(Controller2D controller)
 		{
-
+			
 		}
 	}
 }

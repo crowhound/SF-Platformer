687910d85f080dd211f152736e7ca4e7328a6572
crowhound <jonathan.westfall1@gmail.com>
2023-12-29 18:29:04 -0600

    Removed the Scene events out of game controller. There is a Scene Event Manager now

--- Runtime/Managers/GameManager.cs
+++ Runtime/Managers/GameManager.cs
@@ -1,78 +1,41 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using SF.Events;
+using UnityEngine.Events;
+
 namespace SF
 {
+	/// <summary>
+	/// The current state that is controlling the games input and actions. 
+	/// </summary>
+	public enum GameControlState
+	{
+		Player,
+		SceneChanging,
+		Cutscenes,
+		CameraTransition,
+		TransformTransition // Player being moved within a scene, but has no control over the player. Think teleporting.
+	}
+
+	public class GameController
+	{
+		#region Unity Events
+		public UnityEvent OnGameControlChange;
+		#endregion
+
+		public static GameControlState CurentControlState { get; private set; }
+
+		public static GameControlState PreviousControlState{ get; private set; }
+
+		public static void ChangeControlState(GameControlState gameControlState)
+		{
+			if (gameControlState == CurentControlState) return;
+
+			PreviousControlState = CurentControlState;
+			CurentControlState = gameControlState;
+		}
+	}
+
     public class GameManager : MonoBehaviour
     {
-
-		public static GameManager Instance;
-
-		#region Scene Change Events
-		[SerializeField] private SerializedEvent SceneLoadEvent;
-		[SerializeField] private SerializedEvent SceneDeloadEvent;
-		[SerializeField] private SerializedEvent SceneChangeEvent;
-		#endregion
-
-		#region Starting Lifecycle Functions
-		public void Awake()
-		{
-			if(Instance != null && Instance != this)
-			{
-				Destroy(Instance);
-				return;
-			}
-
-			Instance = this;
-		}
-
-		private void Start()
-		{
-			Init();
-		}
-		#endregion
-
-		private void Init()
-		{
-
-		}
-		#region Scene Events
-		private void OnSceneLoad(Scene scene, LoadSceneMode loadMode)
-		{
-			SceneLoadEvent?.Invoke();
-		}
-		private void OnSceneUnload(Scene scene)
-		{
-			SceneDeloadEvent?.Invoke();
-		}
-		private void OnSceneChanged(Scene previousScene, Scene newScene)
-		{
-			SceneChangeEvent?.Invoke();
-		}
-		#endregion
-		#region Scene Functions
-		/*public void LoadScene(Scene scene, LoadSceneMode loadMode)
-		{
-
-		}
-		*/
-
-		public async Awaitable LoadScene(string sceneName, LoadSceneMode loadMode = LoadSceneMode.Single)
-		{
-			await SceneManager.LoadSceneAsync(sceneName, loadMode);
-		}
-		#endregion
-		private void OnEnable()
-		{
-			SceneManager.sceneLoaded += OnSceneLoad;
-			SceneManager.sceneUnloaded += OnSceneUnload;
-			SceneManager.activeSceneChanged += OnSceneChanged;
-		}
-		private void OnDisable()
-		{
-			SceneManager.sceneLoaded -= OnSceneLoad;
-			SceneManager.sceneUnloaded -= OnSceneUnload;
-			SceneManager.activeSceneChanged -= OnSceneChanged;
-		}
+		public GameController GameController;
 	}
 }

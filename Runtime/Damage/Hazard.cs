c18b65948e2b020bd13794d0c1c2b1495fbc32f7
crowhound <jonathan.westfall1@gmail.com>
2024-02-11 15:32:26 -0600

    [Damager] Added the ability to damage things on collision and not just trigger.

--- Runtime/Damage/Hazard.cs
+++ Runtime/Damage/Hazard.cs
@@ -6,6 +6,14 @@ namespace SF
     {
         public int DamageAmount = 1;
         
+        private void OnCollisionEnter2D(Collision2D collision2D)
+        {
+            if(collision2D.gameObject.TryGetComponent(out IDamagable damagable))
+            {
+                damagable.TakeDamage(DamageAmount);
+            }
+        }
+
         private void OnTriggerEnter2D(Collider2D collider2D)
         {
             if(collider2D.TryGetComponent(out IDamagable damagable))

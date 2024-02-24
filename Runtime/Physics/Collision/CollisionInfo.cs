13218d46443cd70ea7c4e1018875d189157192c9
crowhound <jonathan.westfall1@gmail.com>
2024-02-24 16:38:55 -0600

    Set several values to NonSerialized inn the CollisinInfo.
    these values really don't need to be shown in the inspector or serialized at all so might as well set them.

--- Runtime/Physics/Collision/CollisionInfo.cs
+++ Runtime/Physics/Collision/CollisionInfo.cs
@@ -10,17 +10,17 @@ namespace SF
 		public RaycastHit2D CeilingHit;
 		public RaycastHit2D RightHit;
 		public RaycastHit2D LeftHit;
-
+		
 		public bool IsCollidingRight;
 		public bool IsCollidingLeft;
 		public bool IsCollidingAbove;
 		public bool IsCollidingBelow;
 
 		// The below is for seeing if we were colliding in a direction on the previous frame.
-		public bool WasCollidingRight;
-		public bool WasCollidingLeft;
-		public bool WasCollidingAbove;
-		public bool WasCollidingBelow;
+		[NonSerialized] public bool WasCollidingRight;
+		[NonSerialized] public bool WasCollidingLeft;
+		[NonSerialized] public bool WasCollidingAbove;
+		[NonSerialized] public bool WasCollidingBelow;
 
 		// These are for invoking actions on the frame a new collision takes place.
 		public Action OnCollidedRight;

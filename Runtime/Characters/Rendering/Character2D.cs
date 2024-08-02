c9da8446054b568fa1a6001538ae9cf47cdfbce0
crowhound <jonathan.westfall1@gmail.com>
2024-08-02 12:35:45 -0500

    Added animation fading and has control for states for characters.
    Added
    - A way to force the character animation state. Think knock back on a character forcing them into the knock out state or starting a cutscene.
    
    Updated
    - Updated the SetAnimationState to check if there is a hash needing to be created for a forced animation state.

--- Runtime/Characters/Rendering/Character2D.cs
+++ Runtime/Characters/Rendering/Character2D.cs
@@ -20,7 +20,9 @@ namespace SF.Characters
 		#endregion
 
 		private int AnimationHash => Animator.StringToHash(CharacterState.MovementState.ToString());
-
+		private int _forcedStateHash = 0;
+		private float _animationFadeTime = 0;
+		private bool _isForcedCrossFading = false;
 		#region Lifecycle Functions  
 		private void Awake()
 		{
@@ -50,24 +52,44 @@ namespace SF.Characters
 		}
 		private void SetAnimations()
 		{
-			if(_animator.HasState(0, AnimationHash))
+			if(_isForcedCrossFading)
+			{
+				_animationFadeTime -= Time.deltaTime;
+				if(_animationFadeTime < 0)
+					_isForcedCrossFading = false;
+                return;
+			}
+
+			if(_forcedStateHash != 0 && _animator.HasState(0, _forcedStateHash))
+			{
+				_isForcedCrossFading = true;
+                _animator.CrossFade(_forcedStateHash, _animationFadeTime);
+				_forcedStateHash = 0;
+			}
+			else if(_animator.HasState(0, AnimationHash))
 			{
 				_animator.CrossFade(AnimationHash, 0);
 			}
 		}
 
+		// The 0.3f is the default fade time for Unity's crossfade api.
+		public void SetAnimationState(string stateName, float animationFadeTime = 0.3f)
+		{
+			_forcedStateHash = Animator.StringToHash(stateName);
+            _animationFadeTime = animationFadeTime;
+        }
 		private void SpriteFlip(Vector2 direction)
 		{
 			_spriteRend.flipX = StartedFacingRight
 				? (!(direction.x > 0))
 				: (!(direction.x < 0));
-
         }
 
 		private void OnDirectionChanged(object sender, Vector2 direction)
 		{
 			if(direction.x == 0)
 				return;
+
 			SpriteFlip(direction);
 		}
 	}

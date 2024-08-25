d74064aaf5a00f4c400b225212edf8815398f2c3
crowhound <jonathan.westfall1@gmail.com>
2024-08-25 12:02:53 -0500

    Multiple updates on jump height and jump restrictions.
    Added:
    
    1. The ability to increase jump height based on if the character is running or not.
    2. The Check Ability Requirements overload to restrict when the character can jump.

--- Runtime/Abilities/Characters/Movement Abilities/JumpAbility.cs
+++ Runtime/Abilities/Characters/Movement Abilities/JumpAbility.cs
@@ -8,8 +8,10 @@ namespace SF.Abilities.CharacterModule
 {
     public class JumpAbility : AbilityCore, IInputAbility
     {
-        [Header("Jumping Phjysics")]
+        [Header("Jumping Physics")]
         public float JumpHeight = 12;
+        public float RunningJumpMultiplier = 1.1f;
+        private float CalculatedJumpHeight;
         public int JumpAmount = 1;
         public int JumpsRemaining;
 
@@ -17,15 +19,38 @@ namespace SF.Abilities.CharacterModule
         {
             _controller2d.OnGrounded += ResetJumps;
         }
+
+        protected override bool CheckAbilityRequirements()
+        {
+            if(!_isInitialized || !IsEnabled || _controller2d == null)
+                return false;
+
+            // If we are currently gliding don't jump. 
+            // Due note we could add the ability to jump in mid glide for more movement customization. Maybe a boolean in the Glide called CanGlideJump.
+            if(_controller2d.IsGliding)
+                return false;
+
+            if(JumpsRemaining < 1)
+                return false;
+
+            return true;
+        }
+
         private void OnInputJump(InputAction.CallbackContext context)
 		{
-			if (IsEnabled == false || JumpsRemaining < 1) return;
+            if(!CheckAbilityRequirements()) return;
+
+            CalculatedJumpHeight = _controller2d.IsRunning
+                ? JumpHeight * RunningJumpMultiplier
+                : JumpHeight;
 
             JumpsRemaining--;
 
 			_controller2d.IsJumping = true;
             _controller2d.IsFalling = false;
-            _controller2d.SetVerticalVelocity(JumpHeight);
+
+            // TODO: Only add the running heigh bonus to the first jump.
+            _controller2d.SetVerticalVelocity(CalculatedJumpHeight);
 		}
 
         public void ResetJumps()

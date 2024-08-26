44efa5ffd4718a0282ab44266909fe14f5b93610
crowhound <jonathan.westfall1@gmail.com>
2024-08-26 09:55:51 -0500

    Added the start of the crouch ability for characters
    Note: This is just the start of implementation. It needs more work done.
    The player doesn't have a crawl animation so if you try to move while crouching you just slide in crouch freeze frame animation.

---
+++ Runtime/Abilities/Characters/Movement Abilities/CrouchAbility.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+using SF.InputModule;
+using SF.Physics;
+using System;
+
+namespace SF.AbilityModule.CharacterModule
+{
+    public class CrouchAbility : AbilityCore, IInputAbility
+    {
+        [SerializeField] private Vector2 _colliderResized = new Vector2(.5f,1.5f);
+
+        protected override bool CheckAbilityRequirements()
+        {
+            if(!_isInitialized || !IsEnabled || _controller2d == null)
+                return false;
+
+            // Only crouch if we are grounded.
+            return _controller2d.IsGrounded;
+        }
+
+        private void OnInputCrouch(InputAction.CallbackContext context)
+        {
+            //TODO: Need to do some collider check to make sure the character is not being pushed into a ceiling or something else when uncrouching.
+
+            if(!CheckAbilityRequirements()) return;
+
+            _controller2d.IsCrouching = !_controller2d.IsCrouching;
+
+            if(_controller2d.IsCrouching)
+                _controller2d.ResizeCollider(_colliderResized);
+            else
+                _controller2d.ResetColliderSize();
+        }
+
+        private void OnEnable()
+        {
+            InputManager.Controls.Player.Enable();
+            InputManager.Controls.Player.Crouch.performed += OnInputCrouch;
+        }
+
+        private void OnDisable()
+        {
+            if(InputManager.Instance == null) return;
+            InputManager.Controls.Player.Crouch.performed -= OnInputCrouch;
+        }
+    }
+}

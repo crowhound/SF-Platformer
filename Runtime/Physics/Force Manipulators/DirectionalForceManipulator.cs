372ed39c535a55ff05c970df2893793775e58a72
crowhound <jonathan.westfall1@gmail.com>
2024-11-04 12:41:24 -0600

    Added the ability to do one shot animations on characters being affected by sources of DirectionalForceManipulators.

--- Runtime/Physics/Force Manipulators/DirectionalForceManipulator.cs
+++ Runtime/Physics/Force Manipulators/DirectionalForceManipulator.cs
@@ -9,10 +9,20 @@ namespace SF
     {
         public Vector2 Force;
         public ContactFilter2D ContactFilter;
+        
+        [Header("Animations")]
+        [SerializeField] private bool _hasOneShotAnimation;
+        [SerializeField] private string _animationName;
+        private Animator _animator;
+
+        private void Awake()
+        {
+            if(_hasOneShotAnimation)
+                _animator = GetComponent<Animator>();
+        }
 
         public void OnTriggerEnter2D(Collider2D other)
         {
-
             if((ContactFilter.layerMask & (1 << other.gameObject.layer)) == 0)
                 return;
 
@@ -24,6 +34,10 @@ namespace SF
 
         public void ExtertForce(IForceReciever forceReciever, Vector2 force)
         {
+            if(_hasOneShotAnimation && _animator != null)
+            {
+                _animator.Play("Adding Force",0);
+            }
             forceReciever.SetExternalVelocity(force);
         }
     }

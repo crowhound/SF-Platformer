7c68103c60901c227ecc68fd2b1b88bc68463927
crowhound <jonathan.westfall1@gmail.com>
2024-12-24 03:31:26 -0600

    Added a static reference to the Checkpoint manager.

--- Runtime/CheckPoint/CheckPointManager.cs
+++ Runtime/CheckPoint/CheckPointManager.cs
@@ -14,8 +14,32 @@ namespace SF.SpawnModule
         public CheckPoint StartingCheckPoint;
         public CheckPoint CurrentCheckPoint;
 
+		private static CheckPointManager _instance;
+		public static CheckPointManager Instance
+		{
+			get
+			{
+				if(_instance == null)
+				{
+					GameObject go = new GameObject("Check Point Manager", typeof(CheckPointManager));
+					_instance = go.GetComponent<CheckPointManager>();
+				}
+				return _instance;
+			}
+
+			set
+			{
+				_instance = value;
+			}
+		}
+
+        private void Awake()
+        {
+            Instance = this;
+        }
         private void Start()
         {
+			
 			if(StartingCheckPoint != null && CurrentCheckPoint == null)
 				CurrentCheckPoint = StartingCheckPoint;
 

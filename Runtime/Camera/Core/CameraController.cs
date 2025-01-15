a517d7df44aa42230d6563440194fbafa7faa57b
crowhound <jonathan.westfall1@gmail.com>
2025-01-15 11:49:46 -0600

    Added functions for setting the player camera or changing the collider for camera confiner.
    Side note these are not used yet in code, but are called from UnityEvents on Cinemachine built in components for camera extentions.
    Example the CinemachineTriggerAction component can use it to set the room camera when entering new rooms.

--- Runtime/Camera/Core/CameraController.cs
+++ Runtime/Camera/Core/CameraController.cs
@@ -2,7 +2,7 @@ using Unity.Cinemachine;
 
 using UnityEngine;
 
-namespace SF
+namespace SF.CameraModule
 {
     public class CameraController : MonoBehaviour
     {
@@ -31,12 +31,32 @@ namespace SF
 
             Instance = this;
         }
+
+        public static void SetPlayerCMCamera(CinemachineCamera cmCamera)
+        {
+            if(cmCamera == null)
+                return;
+                
+            Instance.PlayerCamera = cmCamera;
+        }
+
         public static void ChangeCameraConfiner(CinemachineCamera cmCamera)
         {
             if(Instance.PlayerCamera != null)
             {
                 cmCamera.Prioritize();
                 Instance.PlayerCamera = cmCamera;
+            }
+        }
+
+        public static void ChangeCameraConfiner(Collider2D collider2D)
+        {
+
+            if(Instance.PlayerCamera != null)
+            {
+                Debug.Log(Instance.PlayerCamera, Instance.PlayerCamera.gameObject);
+                Instance.PlayerCamera.GetComponent<CinemachineConfiner2D>().BoundingShape2D = collider2D;
+                Instance.PlayerCamera.GetComponent<CinemachineConfiner2D>().InvalidateBoundingShapeCache();
             }
         }
     }
